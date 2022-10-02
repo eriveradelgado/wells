1ca639ee3f56ecf3e5ceb0872d8090ad8e2810bc
edgardrivera <edgardrivera@gmail.com>
2022-10-02 17:51:06 -0400

    Adds platemap_column and platemap_row

--- R/layout.R
+++ R/layout.R
@@ -27,14 +27,66 @@ tibble(well  = range,
 }
 # usage layout_range("A1:C5" = 1)
 
-platemap_column <- function(range,...){
+platemap_column <- function(...){
+
+  list_range <- list(...)
+  names_list_range <- names(list_range)
+
+  # Initialize the vector
+  range <- vector(mode = "list", length = length(list_range))
+  value <- vector(mode = "list", length = length(list_range))
+
+  for(i in 1:length(names_list_range)){
+
+    range[[i]] <- wells(initial_row = "A",
+                        final_row = "H",
+                        initial_column = as.numeric(names_list_range[[i]]),
+                        final_column = as.numeric(names_list_range[[i]]))
+
+    value[[i]] <- rep(list_range[[i]], times = length(range[[i]]))
+
+  }
+
+  range <- as.vector(unlist(range))
+  value <- as.vector(unlist(value))
+
+  tibble::tibble(well  = range,value = value)
 
 }
 
-platemap_row <- function(range,...){
+# platemap_column("1" = "Caffeine",
+#                 "2" = "Ethanol",
+#                 "3" = "Capsaicin")
+
+platemap_row <- function(...){
+  list_range <- list(...)
+  names_list_range <- names(list_range)
+
+  # Initialize the vector
+  range <- vector(mode = "list", length = length(list_range))
+  value <- vector(mode = "list", length = length(list_range))
+
+  for(i in 1:length(names_list_range)){
+
+    range[[i]] <- wells(initial_row = names_list_range[[i]],
+                        final_row = names_list_range[[i]],
+                        initial_column = 1,
+                        final_column = 12)
+
+    value[[i]] <- rep(list_range[[i]], each = length(range[[i]]))
+
+  }
+
+  range <- as.vector(unlist(range))
+  value <- as.vector(unlist(value))
+
+  tibble::tibble(well  = range,value = value)
+}
+
+layout_sequence <- function(...){
 
 }
 
-layout_sequence <- function(range,...){
+platemap_well <- function(){
 
 }
